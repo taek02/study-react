a1cb119c9920f2b4ad461ddfc845a8285997e275
banner <abcdjht123@gmail.com>
2022-08-12 11:12:54 +0900

    3장 컴포넌트 공부

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// 함수형 컴포넌트
+/*import './App.css';
+
+function App() {
+  const name = "react";
+  return <div className="react">{name}</div>;
+}
+export default App; */
+
+
+//클래스형 컴포넌트
+import Say from "./Say";
+
+const App = () => {
+  return <Say/>;
+};
+
+export default App;
---
+++ src/Counter.js
@@ -0,0 +1,47 @@
+import {Component} from 'react';
+
+class Counter extends Component {
+  
+  /* 생성자에 초깃값 지정
+  (constructor(props){
+    super(props);
+    this.state = {
+      number: 0,
+      fixedNumber: 0
+    };
+  }
+  */ 
+
+  //state를 사용해서 초깃값 지정
+  state = {
+    number: 0,
+    fixedNumber: 0
+  };
+
+  render(){
+    const {number, fixedNumber} = this.state;
+    return(
+      <div>
+        <h1>{number}</h1>
+        <h2>바뀌지 않는 값: {fixedNumber}</h2>
+        <button
+          onClick={() => {
+            this.setState(
+              {
+                number: number + 1
+              },
+              () => {
+                console.log("방금 setState가 호출되었습니다.");
+                console.log(this.state);
+              }
+            );
+          }}
+          >
+            +1
+          </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
